1c98e56412fea6d10a5db9116832f7296630dffe
laetitia123 <uwizelaetitia@gmail.com>
2019-09-22 10:40:39 +0200

    comment model has been added

--- app/models.py
+++ app/models.py
@@ -73,97 +73,97 @@ class Pitch(db.Model):
         pitches = Pitch.query.filter_by(id=id).all()
         return pitches
 
-class Comment(db.Model):
-    __tablename__='comments'
+# class Comment(db.Model):
+#     __tablename__='comments'
     
-    id = db.Column(db.Integer,primary_key=True)
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
-    description = db.Column(db.Text)
-
-    
-    def __repr__(self):
-        return f"Comment : id: {self.id} comment: {self.description}"
-
-
-class Upvote(db.Model):
-    __tablename__ = 'upvotes'
-
-    id = db.Column(db.Integer,primary_key=True)
-    upvote = db.Column(db.Integer,default=1)
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-
-    def save_upvotes(self):
-        db.session.add(self)
-        db.session.commit()
-
-
-    def add_upvotes(cls,id):
-        upvote_pitch = Upvote(user = current_user, pitch_id=id)
-        upvote_pitch.save_upvotes()
+#     id = db.Column(db.Integer,primary_key=True)
+#     pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
+#     description = db.Column(db.Text)
 
     
-    @classmethod
-    def get_upvotes(cls,id):
-        upvote = Upvote.query.filter_by(pitch_id=id).all()
-        return upvote
-
-    @classmethod
-    def get_all_upvotes(cls,pitch_id):
-        upvotes = Upvote.query.order_by('id').all()
-        return upvotes
-
-    def __repr__(self):
-        return f'{self.user_id}:{self.pitch_id}'
+#     def __repr__(self):
+#         return f"Comment : id: {self.id} comment: {self.description}"
 
 
+# class Upvote(db.Model):
+#     __tablename__ = 'upvotes'
 
-class Downvote(db.Model):
-    __tablename__ = 'downvotes'
+#     id = db.Column(db.Integer,primary_key=True)
+#     upvote = db.Column(db.Integer,default=1)
+#     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+#     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
-    id = db.Column(db.Integer,primary_key=True)
-    downvote = db.Column(db.Integer,default=1)
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-
-    def save_downvotes(self):
-        db.session.add(self)
-        db.session.commit()
+#     def save_upvotes(self):
+#         db.session.add(self)
+#         db.session.commit()
 
 
-    def add_downvotes(cls,id):
-        downvote_pitch = Downvote(user = current_user, pitch_id=id)
-        downvote_pitch.save_downvotes()
+#     def add_upvotes(cls,id):
+#         upvote_pitch = Upvote(user = current_user, pitch_id=id)
+#         upvote_pitch.save_upvotes()
 
     
-    @classmethod
-    def get_downvotes(cls,id):
-        downvote = Downvote.query.filter_by(pitch_id=id).all()
-        return downvote
+#     @classmethod
+#     def get_upvotes(cls,id):
+#         upvote = Upvote.query.filter_by(pitch_id=id).all()
+#         return upvote
 
-    @classmethod
-    def get_all_downvotes(cls,pitch_id):
-        downvote = Downvote.query.order_by('id').all()
-        return downvote
+#     @classmethod
+#     def get_all_upvotes(cls,pitch_id):
+#         upvotes = Upvote.query.order_by('id').all()
+#         return upvotes
 
-    def __repr__(self):
-        return f'{self.user_id}:{self.pitch_id}'
+#     def __repr__(self):
+#         return f'{self.user_id}:{self.pitch_id}'
+
+
+
+# class Downvote(db.Model):
+#     __tablename__ = 'downvotes'
+
+#     id = db.Column(db.Integer,primary_key=True)
+#     downvote = db.Column(db.Integer,default=1)
+#     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+#     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+#     def save_downvotes(self):
+#         db.session.add(self)
+#         db.session.commit()
+
+
+#     def add_downvotes(cls,id):
+#         downvote_pitch = Downvote(user = current_user, pitch_id=id)
+#         downvote_pitch.save_downvotes()
+
+    
+#     @classmethod
+#     def get_downvotes(cls,id):
+#         downvote = Downvote.query.filter_by(pitch_id=id).all()
+#         return downvote
+
+#     @classmethod
+#     def get_all_downvotes(cls,pitch_id):
+#         downvote = Downvote.query.order_by('id').all()
+#         return downvote
+
+#     def __repr__(self):
+#         return f'{self.user_id}:{self.pitch_id}'
 
     
 
-class Movie:
-    '''
-    Movie class to define Movie Objects
-    '''
+# class Movie:
+#     '''
+#     Movie class to define Movie Objects
+#     '''
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
-        self.title = title
-        self.overview = overview
-        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
+#     def __init__(self,id,title,overview,poster,vote_average,vote_count):
+#         self.id =id
+#         self.title = title
+#         self.overview = overview
+#         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+#         self.vote_average = vote_average
+#         self.vote_count = vote_count
 
 
 
